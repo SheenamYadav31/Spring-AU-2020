5104a6123b1b023923f91686b023455dd9342152
SheenamYadav31 <59680318+SheenamYadav31@users.noreply.github.com>
2020-01-15 09:22:09 +0530

    Update NewComponent.js

--- 13-01-20/Q1 using react/src/NewComponent.js
+++ 13-01-20/Q1 using react/src/NewComponent.js
@@ -19,7 +19,7 @@ class NewComponent extends React.Component {
 
 		console.log(this.state.current_item);
 	}
-	matched = event_1 => {
+	matched(event_1) {
 		//console.log(event_1.target.value);
 		var input_val = event_1.target.value;
 		//console.log(input_val);
@@ -40,27 +40,27 @@ class NewComponent extends React.Component {
 	}
 	render(){
 		const list = this.state.d;
-		//const clickedlist = this.state.current_item;
+		const clickedlist = this.state.current_item;
 		const matched_item = this.state.matched_item;
 		//console.log(current_item);
 		return (
 			<div>
 				<div style={{float:"left", display:"block", position:"relative"}}>
 					<p>Hello from Sheenam</p>
-					<input type="text" placeholder="enter CITY to match..." onChange={this.matched}/>
+					<input type="text" placeholder="enter CITY to match..." onChange={this.matched.bind(this)}/>
 					<div style={{color:"green"}}>
 						<ul>
 							{matched_item.map((city)=>(<li key={city.id}>{city.id}, {city.city} </li>))}
 						</ul>
 					</div>
 					<ul style={{color:"blue"}}>
-						{list.map((dt)=>(<li onClick={()=>this.displayFunction(dt)} key={dt.id}>{dt.id},  {dt.city},  {dt.pollutant_id}, Station: {dt.station}</li>))}
+						{list.map((dt)=>(<li onClick={()=>this.displayFunction(dt)} key={dt.id}>{dt.id}. <b>{dt.city}</b> -> {dt.pollutant_id}, Station: {dt.station}</li>))}
 					</ul>
 				</div>
 				<div style={{width:"550px",color:"red", float:"right", display:"block", position:"absolute",marginLeft:"700px"}}>
 					<label>click on items to see min & max pollutant</label>
 					<ul>
-						{this.state.current_item.map((item)=>(<li key={item.id}>{item.id}  {item.city}  min: {item.pollutant_min}, max: {item.pollutant_max}, </li>))}
+						{clickedlist.map((item)=>(<li key={item.id}>{item.id}  {item.city} -> Pollutant:<b>{item.pollutant_id}</b>  Min: {item.pollutant_min}, Max: {item.pollutant_max}, </li>))}
 					</ul>
 				</div>				
 			</div>
